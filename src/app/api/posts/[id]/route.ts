bdd2b4ed459259820e3d468e5a071de00a84af86
Lilli Nappi <116825146+nappilil@users.noreply.github.com>
2024-12-10 19:06:50 -0500

    update like/dislike for comments & forumpage

--- src/app/api/posts/[id]/route.ts
+++ src/app/api/posts/[id]/route.ts
@@ -2,6 +2,7 @@ import * as context from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 import { auth } from "@/lib/lucia";
 import Post, { type IPost } from "@/lib/models/post";
+import Comment, { type IComment } from "@/lib/models/comment";
 
 export async function GET(request: NextRequest, { params }: { params: { id: string } }) {
 
@@ -53,7 +54,8 @@ export async function POST(request: NextRequest, { params }: { params: { id: str
             if (!post) {
                 return NextResponse.json({ error: "Post not found." }, { status: 404 });
             }
-            const body = await request.json();
+            const body = await request.json(); 
+
             if (body.liked) {
                 if (post.dislikes.includes(session.user.id as string)) {
                     post.dislikes = post.dislikes.filter((id: string) => id !== session.user.id)
@@ -81,7 +83,7 @@ export async function POST(request: NextRequest, { params }: { params: { id: str
                 }
             });
             post.likes = removed;
-            
+
             removed = [];
             post.dislikes.forEach(id => {
                 if (!removed.includes(id)) {
@@ -94,7 +96,58 @@ export async function POST(request: NextRequest, { params }: { params: { id: str
                 $set: post
             }).exec();
             await post.save();
-            return NextResponse.json({ success: true, post }, { status: 200 });
+
+            let comment;
+            if (body.commentId) {
+                comment = (await Comment.findById(body.commentId).exec()) as IComment;
+                
+                if (!comment) {
+                    return NextResponse.json({ error: "Comment not found." }, { status: 404 });
+                }
+    
+                if (body.commentLiked) {
+                    if (comment.dislikes.includes(session.user.id as string)) {
+                        comment.dislikes = comment.dislikes.filter((id: string) => id !== session.user.id)
+                        comment.likes.push(session.user.id as string)
+                    }
+                    else if (comment.likes.includes(session.user.id as string)) {
+                        comment.likes = comment.likes.filter((id: string) => id !== session.user.id)
+                    } else {
+                        comment.likes.push(session.user.id as string)
+                    }
+                } else if (body.commentDisliked) {
+                    if (comment.likes.includes(session.user.id as string)) {
+                        comment.likes = comment.likes.filter((id: string) => id !== session.user.id)
+                        comment.dislikes.push(session.user.id as string)
+                    } else if (comment.dislikes.includes(session.user.id as string)) {
+                        comment.dislikes = comment.dislikes.filter((id: string) => id !== session.user.id)
+                    } else {
+                        comment.dislikes.push(session.user.id as string)
+                    }
+                }
+                let removed: string[] = [];
+                comment.likes.forEach(id => {
+                    if (!removed.includes(id)) {
+                        removed.push(id);
+                    }
+                });
+                comment.likes = removed;
+    
+                removed = [];
+                comment.dislikes.forEach(id => {
+                    if (!removed.includes(id)) {
+                        removed.push(id);
+                    }
+                });
+                comment.dislikes = removed;
+    
+                await Comment.findByIdAndUpdate(body.commentId, {
+                    $set: comment
+                }).exec();
+                await comment.save();
+
+            }       
+            return NextResponse.json({ success: true, post, comment }, { status: 200 });
         } catch (e) {
             console.log(e)
             return NextResponse.json({ error: e }, { status: 404 });
